bdf106f5bc8886f5177c6840ad46a67b6df7bc72
imbhargav5 <imbhargav5@gmail.com>
2020-08-16 16:50:52 +0530

    Update mustache

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 const main = require("./main");
 const TOP_CONTRIBUTIONS = [];
 const NOTABLE_CONTRIBUTIONS = []
+const githubSummary = `<h3>Hi I am Bhargav.</h3>`
 const githubContributions = [TOP_CONTRIBUTIONS,NOTABLE_CONTRIBUTIONS ]
-main({githubContributions});
+main({githubContributions, githubSummary});
--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const Mustache = require('mustache');
 
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
-async function generateReadMe({githubContributions}) {  
+async function generateReadMe({githubContributions, githubSummary}) {  
   const template = await fs.readFile(MUSTACHE_MAIN_DIR);
   const [topContributions, notableContributions] = githubContributions;
   const DATA = {
       topContributions,
-      notableContributions
+      notableContributions,
+      githubSummary
   }
   const output = Mustache.render(template.toString(), DATA);
   await fs.writeFile('README.md', output);
