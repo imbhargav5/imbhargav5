c5784d7e0db80b8c2b3c37e1f05aff43df4a04e8
imbhargav5 <imbhargav5@gmail.com>
2020-08-16 15:12:01 +0530

    Fix typo

--- action.js
+++ action.js
@@ -1,5 +1,5 @@
 const main = require("./main");
-const TOP_CONTRIBUTIONS = JSON.stringify(process.env.TOP_CONTRIBUTIONS);
-const NOTABLE_CONTRIBUTIONS = JSON.stringify(process.env.NOTABLE_CONTRIBUTIONS)
+const TOP_CONTRIBUTIONS = JSON.parse(process.env.TOP_CONTRIBUTIONS);
+const NOTABLE_CONTRIBUTIONS = JSON.parse(process.env.NOTABLE_CONTRIBUTIONS)
 const githubContributions = [TOP_CONTRIBUTIONS,NOTABLE_CONTRIBUTIONS ]
 main({githubContributions});
