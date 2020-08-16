7819b696021fe51ea9d266141b0f08ede11f5c41
imbhargav5 <imbhargav5@gmail.com>
2020-08-16 15:07:36 +0530

    Updates

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ require('dotenv').config();
 const main = require("./main");
 const TOP_CONTRIBUTIONS = [];
 const NOTABLE_CONTRIBUTIONS = []
-
 const githubContributions = [TOP_CONTRIBUTIONS,NOTABLE_CONTRIBUTIONS ]
 main({githubContributions});
