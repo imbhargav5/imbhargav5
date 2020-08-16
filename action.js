fdbcec5474b726c9554ca238eb85918e755a8f59
imbhargav5 <imbhargav5@gmail.com>
2020-08-16 17:15:48 +0530

    Update

--- action.js
+++ action.js
@@ -1,6 +1,6 @@
 const main = require("./main");
 const TOP_CONTRIBUTIONS = JSON.parse(process.env.TOP_CONTRIBUTIONS);
 const NOTABLE_CONTRIBUTIONS = JSON.parse(process.env.NOTABLE_CONTRIBUTIONS)
-const GITHUB_SUMMARY = JSON.parse(process.env.GITHUB_SUMMARY)
+const githubSummary = JSON.parse(process.env.GITHUB_SUMMARY)
 const githubContributions = [TOP_CONTRIBUTIONS,NOTABLE_CONTRIBUTIONS ]
-main({githubContributions});
+main({githubContributions, githubSummary});
