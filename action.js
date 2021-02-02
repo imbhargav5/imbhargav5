c7c94863943be34cde51ed1908200079c1a549ad
imbhargav5 <imbhargav5@gmail.com>
2021-02-02 16:21:29 +0530

    Update readme

--- action.js
+++ action.js
@@ -3,5 +3,6 @@ const TOP_CONTRIBUTIONS = JSON.parse(process.env.TOP_CONTRIBUTIONS);
 const NOTABLE_CONTRIBUTIONS = JSON.parse(process.env.NOTABLE_CONTRIBUTIONS)
 const githubSummary = JSON.parse(process.env.GITHUB_SUMMARY)
 const mostRecentBlogPosts = JSON.parse(process.env.BLOG_POSTS)
+const hashnodeProfile = JSON.parse(process.env.HASHNODE_PROFILE)
 const githubContributions = [TOP_CONTRIBUTIONS,NOTABLE_CONTRIBUTIONS ]
-main({githubContributions, githubSummary, mostRecentBlogPosts});
+main({githubContributions, githubSummary, mostRecentBlogPosts, hashnodeProfile});
--- main.js
+++ main.js
@@ -4,14 +4,21 @@ const Mustache = require('mustache');
 
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
-async function generateReadMe({githubContributions, githubSummary, mostRecentBlogPosts}) {  
+async function generateReadMe({githubContributions, githubSummary, mostRecentBlogPosts, hashnodeProfile}) {  
   const template = await fs.readFile(MUSTACHE_MAIN_DIR);
   const [topContributions, notableContributions] = githubContributions;
+  try{
+    const hashnodePosts = hashnodeProfile.user.publication.posts
+    console.log(hashnodePosts)
+  }catch(err){
+    console.log(hashnodePosts)
+  }
   const DATA = {
       topContributions,
       notableContributions,
       githubSummaryMarkdown: githubSummary.markdown || "",
-      mostRecentBlogPosts
+      mostRecentBlogPosts,
+      hashnodeProfile
   }
   const output = Mustache.render(template.toString(), DATA);
   await fs.writeFile('README.md', output);
