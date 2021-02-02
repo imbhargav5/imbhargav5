42c30edaed6aaa87c96889101878eb72adb0b289
imbhargav5 <imbhargav5@gmail.com>
2021-02-02 16:40:50 +0530

    Update readme

--- main.js
+++ main.js
@@ -7,18 +7,14 @@ const MUSTACHE_MAIN_DIR = './main.mustache';
 async function generateReadMe({githubContributions, githubSummary, mostRecentBlogPosts, hashnodeProfile}) {  
   const template = await fs.readFile(MUSTACHE_MAIN_DIR);
   const [topContributions, notableContributions] = githubContributions;
-  try{
-    const hashnodePosts = hashnodeProfile.user.publication.posts
-    console.log(hashnodePosts)
-  }catch(err){
-    console.log(hashnodePosts)
-  }
+  
   const DATA = {
       topContributions,
       notableContributions,
       githubSummaryMarkdown: githubSummary.markdown || "",
       mostRecentBlogPosts,
-      hashnodeProfile
+      hashnodeUser: hashnodeProfile.user,
+      hashnodePosts: hashnodeProfile.user.publication.posts
   }
   const output = Mustache.render(template.toString(), DATA);
   await fs.writeFile('README.md', output);
